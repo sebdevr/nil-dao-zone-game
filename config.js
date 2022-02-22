2cc457fad1025629f101c641a7e8877168f96a1a
seb <76062949+sebfk@users.noreply.github.com>
2022-02-22 13:51:50 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,8 @@ module.exports = {
     mongodb: "mongodb+srv://Seb:p9gwJg61uXfJkce8@cluster0.kbxbd.mongodb.net/NIL9000?retryWrites=true&w=majority",
     guildID: "884489381510021152",
     announcements: "939872422709059655",
-    time: 36000000, // HOURS * MINS * SECONDS * MSECONDS
+    time: 36000000, // HOURS * MINS * SECONDS * MSECONDS (TIME IN MILESECONDS)
     targetNumber: 333, // TARGET NUMBERS
+    delayTime: 36000000, // HOURS * MINS * SECONDS * MSECONDS (TIME IN MILESECONDS)
     GameChannel: "940682846081806486" // GAME CHANNEL ID
 }
